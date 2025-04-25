be8f5afc03067cc77b6b58a696308f0433378716
PedroZxK <pedrogabrielxx268@gmail.com>
2025-04-24 21:24:58 -0300

    Criação das Páginas Iniciais.

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+include 'conexao.php';
+include 'validacao.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <h1>Home</h1>
+</body>
+
+</html>
